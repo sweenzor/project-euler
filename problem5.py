3e99b08e47b72e366ec5e830547be1bbbab300fe
mattswe <mattswe@gmail.com>
2011-02-01 22:49:35 -0800

    outputting some times to help optimize

--- problem5.py
+++ problem5.py
@@ -2,15 +2,22 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-divisor = 10
+import time
 
-count = 1
-while True:
-	divisor_count = 0
-	for div in range(1,divisor+1):
-		if count % div == 0:
-			divisor_count = divisor_count + 1
-	if divisor_count == divisor:
-		print count
-		break
-	count = count + 1
+
+for divisor in range(30):
+	mark = time.time()
+
+	#divisor = 10
+
+	count = 1
+	while True:
+		divisor_count = 0
+		for div in range(1,divisor+1):
+			if count % div == 0:
+				divisor_count = divisor_count + 1
+		if divisor_count == divisor:
+			break
+		count = count + 1
+
+	print divisor,'\t','%5f' % (time.time()-mark),'\t',count
